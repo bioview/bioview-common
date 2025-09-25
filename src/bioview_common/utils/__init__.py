52f981acc21b27d731b365ba9c4cea0e15b498b3
Aakash Kapoor <113218196+meowkash@users.noreply.github.com>
2025-09-25 18:59:48 -0400

    Add utility functions for easier logging

--- src/bioview_common/utils/__init__.py
+++ src/bioview_common/utils/__init__.py
@@ -1,8 +1,10 @@
 from .network import is_local_request, get_ip, get_app_info, validate_message_format
+from .logs import log_print
 
 __all__ = [
     "is_local_request", 
     "get_ip", 
     "get_app_info",
-    "validate_message_format"
+    "validate_message_format",
+    "log_print"
 ]
